b2d202eccd91b47cf4944da075d2a81ea8d6f0c4
EC2 Default User <ec2-user@ip-172-31-89-53.ec2.internal>
2020-03-15 00:53:15 +0000

    Initial commit of Planets site

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+//routes
+app.get("/", function(req, res){
+    res.render("index.html");
+});
+
+app.get("/mercury", function(req, res){
+    res.render("mercury.html");
+});
+
+app.get("/venus", function(req, res) {
+    res.render("venus.html");
+});
+//listener
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Running Express Server...");
+});
