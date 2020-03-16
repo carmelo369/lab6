77f24be2788e0b3bc99a9ece0bc3977370d62281
EC2 Default User <ec2-user@ip-172-31-89-53.ec2.internal>
2020-03-16 06:03:52 +0000

    Added all planets, added navigation menu, and bootstrap

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 app.engine('html', require('ejs').renderFile);
 app.use(express.static("public"));
+app.use(express.static("css"));
+
 //routes
 app.get("/", function(req, res){
     res.render("index.html");
@@ -14,6 +16,31 @@ app.get("/mercury", function(req, res){
 app.get("/venus", function(req, res) {
     res.render("venus.html");
 });
+
+app.get("/earth", function(req, res) {
+    res.render("earth.html");
+});
+
+app.get("/mars", function(req, res) {
+    res.render("mars.html");
+});
+
+app.get("/jupiter", function(req, res) {
+    res.render("jupiter.html");
+});
+
+app.get("/saturn", function(req, res) {
+    res.render("saturn.html");
+});
+
+app.get("/uranus", function(req, res) {
+    res.render("uranus.html");
+});
+
+app.get("/neptune", function(req, res) {
+    res.render("neptune.html");
+});
+
 //listener
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Running Express Server...");
